5326e428874df900c309e3bce621179442f5ddd9
Sarah-Zacharias <szacharias@student.cvtc.edu>
2025-04-24 00:18:29 -0500

    patient/provider updates

--- controllers/provider_controller.php
+++ controllers/provider_controller.php
@@ -18,6 +18,8 @@ class ProviderController {
 
     // Load provider dashboard
     public function index($provider_id) {
+        error_reporting(E_ALL);
+        ini_set('display_errors', 1);
         var_dump($provider_id); // Debug ID being passed
         die();
     
